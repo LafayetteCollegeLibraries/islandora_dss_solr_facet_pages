36c4d7d832d25a8ba3a5917335ac067852f22496
jrgriffiniii <griffinj@lafayette.edu>
2014-02-13 15:26:11 -0500

    DSS-126 #time 4m Restoring the CSS properties for the Slider controls to their default values

--- js/date_range_slider.js
+++ js/date_range_slider.js
@@ -1071,15 +1071,15 @@ SolrQuery.prototype = {
 			    $handles = $(this).children('.ui-slider-handle');
 			    $($handles[0]).addClass('ui-slider-left');
 			    $($handles[1]).addClass('ui-slider-right');
-
-			    $($handles[0]).css('left', '-5%');
+			    
+			    //$($handles[0]).css('left', '-5%');
 			    //$($handles[0]).css('right', 'inherit');
 
 			    /*
 			    $($handles[1]).css('left', 'inherit');
 			    $($handles[1]).css('right', '0%');
 			    */
-			    $($handles[1]).css('left', '104%');
+			    //$($handles[1]).css('left', '104%');
 			    //$($handles[1]).css('right', '0%');
 			},
 		    };
